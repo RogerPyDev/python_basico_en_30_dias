6becd3f5ab8f69daad4a2662524f7b29491d53b8
Roger Grajales <70453253+RoGraDev@users.noreply.github.com>
2024-11-21 22:50:38 -0500

    Create dia_0.py
    Archivo día 0 creado.
    Primer ejercicio del día 0, resuelto.

---
+++ dia_0.py
@@ -0,0 +1,9 @@
+"""
+- ***Ejercicio 1: Tu primer mensaje***
+    1. Escribe un programa que muestre en pantalla el mensaje: `"Hola, Python!"`.
+    2. Ahora cambia el mensaje para que diga `"Estoy aprendiendo Python!"`.
+    3. Asegúrate de que cada mensaje esté entre comillas.
+"""
+
+print("Hola, Python!")
+print("Estoy aprendiendo Python!")
