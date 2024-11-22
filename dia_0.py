45917c1b1bb599d031abc833029d3aa84b51f7f7
Roger Grajales <70453253+RoGraDev@users.noreply.github.com>
2024-11-22 06:48:06 -0500

    Update dia_0.py
    Ejercicio 2 del día 0
    Solución del ejercicio 2 del día 0

--- dia_0.py
+++ dia_0.py
@@ -1,9 +1,20 @@
 """
-- ***Ejercicio 1: Tu primer mensaje***
+Ejercicio 1: Tu primer mensaje
     1. Escribe un programa que muestre en pantalla el mensaje: `"Hola, Python!"`.
     2. Ahora cambia el mensaje para que diga `"Estoy aprendiendo Python!"`.
     3. Asegúrate de que cada mensaje esté entre comillas.
 """
-
+# Solución:
 print("Hola, Python!")
 print("Estoy aprendiendo Python!")
+
+
+"""
+Ejercicio 2: Números y texto
+    1. Intenta imprimir un número, por ejemplo, `100`, sin comillas.
+    2. Luego, intenta imprimirlo con comillas, como `"100"`.
+    3. Observa la diferencia en los resultados.
+"""
+# Solución:
+print(100)    # Número sin comillas
+print("100")  # Número como texto
